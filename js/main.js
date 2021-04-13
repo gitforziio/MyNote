fe35aac749061d3d4794cace697e52f7bdbd6888
Roomcar <noteforziio@163.com>
2021-04-14 04:03:14 +0800

    🚀

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@ var the_vue = new Vue({
     el: '#app',
     data: {
         "app_name": "MyNote",
-        "fields": ["lean_cloud_keys", "user", "settings", "tabs", "ui"],
+        "fields": ["lean_cloud_keys", "user", "settings", "status", "ui"],
         //
+        "hash": "",
         "lean_cloud_keys": {
             appId: "",
             appKey: "",
@@ -20,14 +21,16 @@ var the_vue = new Vue({
             logged_in: false,
             current_page: 0,
             current_tab: 0,
-            ready: false,
+            loginning: false,
         },
+        "ready": false,
         "settings": {
             remember_user: true,
             dark_mode: false,
+            dark_mode_follow_system: true,
         },
         //
-        "pages": ["login", "main", "welcome", "settings"],
+        "pages": ["login", "main", "welcome", "settings", "add_post_gzh"],
         "tabs": [
             {
                 name: "notes",
@@ -38,9 +41,22 @@ var the_vue = new Vue({
                 show_num: true,
             },
             {
+                name: "tools",
+                // icon: "gem",
+                // icon: "hdd",
+                // icon: "heart",
+                // icon: "star-half",
+                // icon: "star",
+                icon: "lightbulb",
+                idx: 1,
+                active: false,
+                msg_num: 0,
+                show_num: false,
+            },
+            {
                 name: "me",
                 icon: "user",
-                idx: 1,
+                idx: 2,
                 active: false,
                 msg_num: 0,
                 show_num: false,
@@ -53,6 +69,14 @@ var the_vue = new Vue({
             toptips_last_idx: 1,
             toptips: [],
         },
+        "tools_gzh": {
+            url: "",
+            is_analyzing: false,
+            is_analyzed: false,
+            result: null,
+            result_text: "",
+            result_obj: {},
+        },
         //
     },
     computed: {
@@ -60,6 +84,10 @@ var the_vue = new Vue({
             let self = this;
             return self.settings.dark_mode;
         },
+        // hash: function() {
+        //     let self = this;
+        //     return location.hash;
+        // },
     },
 
 
@@ -67,15 +95,49 @@ var the_vue = new Vue({
 
         go_tab: function(x) {
             let self = this;
+            location.hash=`#${self.tabs[x].name}`;
             self.tabs.forEach(pp => {
                 pp.active = pp.idx == x;
             });
             self.ui.tab_current = x;
-            location.hash=`#${self.tabs[x].name}`;
+            self.status.current_page = 1;
+        },
+
+        go_hash: function(hash) {
+            let self = this;
+            console.log(`go to: ${hash}`);
+            let _map = {
+                'notes': function() {
+                    self.go_tab(0);
+                },
+                'tools': function() {
+                    self.go_tab(1);
+                },
+                'me': function() {
+                    self.go_tab(2);
+                },
+                "page-add_post_gzh": function() {
+                    self.tools_gzh= {
+                        url: "",
+                        is_analyzing: false,
+                        is_analyzed: false,
+                        result: null,
+                        result_text: "",
+                        result_obj: {},
+                    };
+                    self.status.current_page = 4;
+                },
+            };
+            if (hash in _map) {
+                _map[hash]();
+            } else {
+                self.go_hash(`notes`);
+            };
         },
 
         login: function() {
             let self = this;
+            self.status.loginning = true;
             if (!self.status.lc_initiated) {
                 try {
                     LC.init(self.lean_cloud_keys);
@@ -86,6 +148,7 @@ var the_vue = new Vue({
             }
             LC.User.login(self.user.username, self.user.password)
             .then((x) => {
+                self.status.loginning = false;
                 self.push_toast('success', `你好，${LC.User.current().data.username}，登录成功啦！`, 1000);
                 // self.status.logged_in = true;
                 self.user.password = '';
@@ -105,7 +168,108 @@ var the_vue = new Vue({
             // self.refresh();
         },
 
+        tools_gzh_check_url() {
+            let self = this;
+            return 0 == self.tools_gzh.url.search(/^(https?:\/\/)?mp\.weixin\.qq\.com/);
+        },
 
+        tools_gzh_analyze() {
+            let self = this;
+            let setting = {
+                "meta": [
+                    {
+                        "selector": "h2#activity-name",
+                        "output_map": {
+                            "class": "meta",
+                            "meta_key": "title",
+                            "meta_value": "__text"
+                        }
+                    },
+                    {
+                        "selector": "#profileBt #js_name",
+                        "output_map": {
+                            "class": "meta",
+                            "meta_key": "channel",
+                            "meta_value": "__text"
+                        }
+                    },
+                    {
+                        "selector": "meta[name=author]",
+                        "output_map": {
+                            "class": "meta",
+                            "meta_key": "author",
+                            "meta_value": "@content"
+                        }
+                    },
+                    {
+                        "selector": "meta[name=description]",
+                        "output_map": {
+                            "class": "meta",
+                            "meta_key": "description",
+                            "meta_value": "@content"
+                        }
+                    }//,
+                    // {
+                    //     "selector": "meta[property]",
+                    //     "output_map": {
+                    //         "class": "meta",
+                    //         "meta_key": "@property",
+                    //         "meta_value": "@content"
+                    //     }
+                    // }
+                ],
+                "content": [
+                    {
+                        "selector": "#js_content",
+                        "output_map": {
+                            "class": "article_field",
+                            "meta_key": "abstract",
+                            "meta_value": "__text_abstract"
+                        }
+                    }//,
+                    // {
+                    //     "selector": "#js_content",
+                    //     "output_map": {
+                    //         "class": "article_field",
+                    //         "meta_key": "full_content",
+                    //         "meta_value": "__strings"
+                    //     }
+                    // }
+                ]//,
+                // "image": [
+                //     {
+                //         "selector": "img[data-src]",
+                //         "output_map": {
+                //             "class": "image",
+                //             "href": "@data-src"
+                //         }
+                //     }
+                // ]
+            };
+            // let setting_string = JSON.stringify(setting);
+            let paramsJson = {
+                url: self.tools_gzh.url,
+                setting: setting,
+            }
+            self.tools_gzh.is_analyzing = true;
+            LC.Cloud.run('spy', paramsJson).then((result)=>{
+                self.tools_gzh.result = result;
+                self.tools_gzh.result_obj = {
+                    title: result.meta[0].meta_value,
+                    channel: result.meta[1].meta_value,
+                    author: result.meta[2].meta_value,
+                    description: result.meta[3].meta_value,
+                    abstract: result.content[0].meta_value,
+                };
+                self.tools_gzh.result_text = JSON.stringify(self.tools_gzh.result_obj);
+                self.tools_gzh.is_analyzing = false;
+                self.tools_gzh.is_analyzed = true;
+                console.log(self.tools_gzh.result);
+            }).catch(({ error }) => {
+                self.tools_gzh.is_analyzing = false;
+                self.push_toast('danger', `分析时发生错误：「${error}」。`);
+            });
+        },
 
 
 
@@ -227,11 +391,14 @@ var the_vue = new Vue({
             };
         },
     },
-    // watch: {
-    //     dark_mode: function() {
-    //         document.querySelector('body').setAttribute('data-weui-theme', self.dark_mode?'dark':'light');
-    //     },
-    // },
+    watch: {
+        hash: function(val, oldVal) {
+            let self = this;
+            if (val!="#"&&val[0]=="#") {
+                self.go_hash(val.slice(1,val.length));
+            };
+        },
+    },
     beforeCreate() {
         let self = this;
     },
@@ -244,20 +411,42 @@ var the_vue = new Vue({
                 self.status.lc_initiated = true;
                 // self.push_toptip('success', `LeanCloud已自动初始化`, 2000);
             } catch(error) {
-                // self.push_toptip('warn', `LeanCloud自动初始化出现问题`, 2000);
+                self.push_toptip('warn', `LeanCloud自动初始化出现问题`, 2000);
             };
         };
         if (self.status.lc_initiated) {self.status.logged_in = LC.User.current() ? true : false;};
+        if( ("onhashchange" in window) && ((typeof document.documentMode==="undefined") || document.documentMode==8)) {
+            window.onhashchange = function() {
+                // self.push_toptip('warn', `浏览器能够监听 hash 变化`, 2000);
+                self.hash = location.hash;
+            };
+        } else {
+            setInterval(function() {
+                // self.push_toptip('warn', `启用了定时检查 hash 的任务`, 2000);
+                self.hash = location.hash;
+            }, 150);
+        };
         if (self.status.logged_in) {
             self.push_toast('success', `你好，${LC.User.current().data.username}，欢迎回来！`, 1000);
-            self.status.current_page = 1;
+            // self.status.current_page = 1;
+            if (self.hash!="#"&&self.hash[0]=="#") {
+                self.go_hash(self.hash.slice(1,self.hash.length));
+            };
         };
-        document.querySelector('body').setAttribute('data-weui-theme', self.settings.dark_mode?'dark':'light');
-        self.status.ready = true;
+        if (self.settings.dark_mode_follow_system) {
+            document.querySelector('body').removeAttribute('data-weui-theme');
+        } else {
+            document.querySelector('body').setAttribute('data-weui-theme', self.settings.dark_mode?'dark':'light');
+        };
+        self.ready = true;
     },
     updated() {
         let self = this;
-        document.querySelector('body').setAttribute('data-weui-theme', self.dark_mode?'dark':'light');
+        if (self.settings.dark_mode_follow_system) {
+            document.querySelector('body').removeAttribute('data-weui-theme');
+        } else {
+            document.querySelector('body').setAttribute('data-weui-theme', self.settings.dark_mode?'dark':'light');
+        };
         if (self.status.lc_initiated) {self.status.logged_in = LC.User.current() ? true : false;};
         self.saveDataToLocalStorage();
     },
